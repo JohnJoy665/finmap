2d692b6db0b8325d7d731cb8a5ad81de5847e497
Evgemiy Larin <e.larin@websoft.ru>
2026-04-26 17:37:42 +0200

    Закончил тестовую авторизацию

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,23 +1,9 @@
-import { useEffect } from "react";
 import { useAuthStore } from "../../store/authStore";
-import { useNavigate } from "react-router-dom";
 
 function Header() {
-    const isAuth = useAuthStore((state) => state.isAuth);
     const logout = useAuthStore((state) => state.logout);
-    const navigate = useNavigate()
 
-    useEffect(()=> {
-        if (!isAuth) {
-            navigate('/')
-        }
-    }, [logout, isAuth])
-
-    function handleExit() {
-        logout();
-    }
-
-    return <button onClick={handleExit}>Выйти</button>
+    return <button onClick={logout}>Выйти</button>
 }
 
 export default Header;
--- client/src/pages/main/Main.tsx
+++ client/src/pages/main/Main.tsx
@@ -1,14 +1,12 @@
 import { Outlet } from "react-router-dom";
 import TwoPanels from "../../layouts/TwoPanels/TwoPanels";
 import RightPanel from "../../layouts/RightPanel";
-import { Header } from "antd/es/layout/layout";
 
 function Main() {
   return (
     <TwoPanels
       left={
         <>
-          <Header />
           <Outlet />
         </>
       }
