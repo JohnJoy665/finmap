e158cc53b32c30b1f0aaba101fe155d5596b2cd7
Evgemiy Larin <e.larin@websoft.ru>
2026-04-27 15:52:13 +0200

    Добавил логин

--- client/src/components/login/Login.tsx
+++ client/src/components/login/Login.tsx
@@ -1,29 +1,78 @@
-import { useEffect } from "react";
-import { useAuthStore } from "../../store/authStore";
+import { Button, Form, Input } from "antd";
 import { useNavigate } from "react-router-dom";
+import { useAuthStore } from "../../store/authStore";
+import style from "./Login.module.css";
+import type { RuleObject } from "antd/es/form";
+
+type LoginFormValues = {
+  email: string;
+  password: string;
+};
 
 function Login() {
   const login = useAuthStore((state) => state.login);
-  const isAuth = useAuthStore((state) => state.isAuth);
-  
   const navigate = useNavigate();
 
-  useEffect(() => {
-    if (isAuth) {
-      navigate("/app/operations");
-    }
-  }, [isAuth, navigate]);
+  function handleSubmit(values: LoginFormValues) {
+    console.log(values);
 
-  function handleLogin() {
     login();
+    navigate("/app/operations");
   }
 
+  function validateConfirmPassword(
+    getFieldValue: (name: string) => string
+  ): RuleObject {
+    return {
+      validator(_, value) {
+        if (!value || getFieldValue("password") === value) {
+          return Promise.resolve();
+        }
+
+        return Promise.reject(new Error("Пароли не совпадают"));
+      },
+    };
+  }
 
   return (
-    <>
-      <p>Логин</p>
-      <button onClick={handleLogin}>Войти</button>
-    </>
+    <div className={style.container}>
+      <Form className={style.form} layout="vertical" onFinish={handleSubmit}>
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            { required: true, message: "Введите email" },
+            { type: "email", message: "Некорректный email" },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Пароль"
+          name="password"
+          rules={[{ required: true, message: "Введите пароль", min: 5 }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          label="Повторите пароль"
+          name="confirmPassword"
+          dependencies={["password"]}
+          rules={[
+            { required: true, message: "Повторите пароль", min: 5 },
+            ({ getFieldValue }) => validateConfirmPassword(getFieldValue),
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Button type="primary" htmlType="submit" block>
+          Войти
+        </Button>
+      </Form>
+    </div>
   );
 }
 
